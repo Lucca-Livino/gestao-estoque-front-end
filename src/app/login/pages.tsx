406067b98ea24035384f4ac6275046357b61c7c4
Deivid Luiz Costa Pereira <pdeiviluiz@gmail.com>
2025-10-07 22:22:30 -0400

    Cards de login

--- src/app/login/pages.tsx
+++ src/app/login/pages.tsx
@@ -1,23 +1,32 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Card, CardTitle, CardHeader, CardAction, CardContent,CardDescription,CardFooter } from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
 
 
 
 export default function LoginPage() {
   return (
     <div className="font-sans flex items-center justify-center h-screen">
-    <Card className="w-[644px] h-[626px] items-center justify-center">
-  <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-    <CardAction>Card Action</CardAction>
+    <Card className="w-[645px] h-[626px] flex-col items-center ">
+  <CardHeader className="flex-col items-center ">
+    <CardTitle className="text-3xl ">Login</CardTitle>
   </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
+  <CardContent className="mt-20">
+    <p className="text-xl font-sans">Matricula*</p>
+    <Input className="w-[481px] h-[60px]"/>    
   </CardContent>
+  <CardContent className="">
+     <p className="text-xl font-sans">Senha*</p>
+    <Input className="w-[481px] h-[60px]"  />
+  </CardContent>
+  <Checkbox className="mt-4"/>Lembrar-me
   <CardFooter>
-    <p>Card Footer</p>
+   <Button className="w-[481px] h-[60px] bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+      Entrar
+    </Button>
   </CardFooter>
 </Card>
     </div>
